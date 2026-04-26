61ba686f3ab459e9e1d86f51f01ed74bd4b875d3
mcabcam23-ui <mcabcam23@gmail.com>
2026-04-26 21:20:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -166,8 +166,8 @@ function renderKeySignatures() {
     const { baseLeft, step } = accidentalLayout(accidentalType);
     const mobileDrop = isMobile
       ? accidentalType === "sharp"
-        ? 7
-        : 12
+        ? 5
+        : 14
       : 0;
 
     for (let i = 0; i < count; i += 1) {
